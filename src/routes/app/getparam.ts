fc02f12ad39f24d5370ffc6bf2978e03c829d0d8
Siraj01576 <sarfaraz.8.kh@gmail.com>
2024-09-01 17:11:25 +0500

    Fixes issue #419: Improve app by adding working params and fix the filter when switch to github or eddie

---
+++ src/routes/app/getparam.ts
@@ -0,0 +1,20 @@
+// getparam.ts
+export function para(): string[] {
+    if (typeof window !== 'undefined') {
+      // Extract query parameters from the URL
+      const urlParams = new URLSearchParams(window.location.search);
+      const filter = urlParams.get('filter');
+  
+      if (filter) {
+        // Decode URL-encoded string
+        const decodedFilter = decodeURIComponent(filter);
+        const arr = decodedFilter.split(',');
+        console.log(arr);
+        return arr;
+      }
+    }
+  
+    // Return an empty array if no valid parameters are found
+    return [];
+  }
+  
